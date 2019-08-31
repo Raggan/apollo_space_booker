131fb654f783466add526166cf88ec42d6978cc6
Simon Holdorf <simon.holdorf@gmail.com>
2019-08-31 07:29:06 +0200

    local state

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,20 @@
 import { ApolloClient } from "apollo-client";
-import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import Login from "./pages/login";
+import injectStyles from "./styles";
 import React from "react";
 import ReactDOM from "react-dom";
 import Pages from "./pages";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
+import { resolvers, typeDefs } from "./resolvers";
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -18,7 +28,9 @@ const client = new ApolloClient({
     headers: {
       authorization: localStorage.getItem("token")
     }
-  })
+  }),
+  typeDefs,
+  resolvers
 });
 
 cache.writeData({
@@ -28,9 +40,15 @@ cache.writeData({
   }
 });
 
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
+injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById("root")
 );
